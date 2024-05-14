4073d7088daa85170026161318117f7603cac292
Prabhum2002 <shamugaprabhum@gmail.com>
2024-05-14 18:18:20 +0530

    Initial Changes

---
+++ Client_Bytes_ConsoleApp/Client_Bytes_ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+IPAddress ipAddress = IPAddress.Parse("172.16.16.19");
+int port = 3003;
+int bufferSize = 1024;
+
+TcpClient client = new TcpClient();
+NetworkStream netStream;
+
+// Connect to server
+try
+{
+    client.Connect(new IPEndPoint(ipAddress, port));
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+netStream = client.GetStream();
+
+// Read bytes from image
+byte[] data = File.ReadAllBytes("C:\\Users\\BI1335\\Pictures\\Screenshots\\Screenshot (14).png");
+
+// Build the package
+byte[] dataLength = BitConverter.GetBytes(data.Length);
+byte[] package = new byte[4 + data.Length];
+dataLength.CopyTo(package, 0);
+data.CopyTo(package, 4);
+
+// Send to server
+int bytesSent = 0;
+int bytesLeft = package.Length;
+
+while (bytesLeft > 0)
+{
+
+    int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+    netStream.Write(package, bytesSent, nextPacketSize);
+    bytesSent += nextPacketSize;
+    bytesLeft -= nextPacketSize;
+
+}
+
+// Clean up
+netStream.Close();
+client.Close();
---
+++ Server_Bytes_ConsoleApp/Server_Bytes_ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+
+TcpListener listen = new TcpListener(3003);
+TcpClient client;
+int bufferSize = 1024;
+NetworkStream netStream;
+int bytesRead = 0;
+int allBytesRead = 0;
+
+// Start listening
+listen.Start();
+
+// Accept client
+client = listen.AcceptTcpClient();
+netStream = client.GetStream();
+
+// Read length of incoming data
+byte[] length = new byte[4];
+bytesRead = netStream.Read(length, 0, 4);
+int dataLength = BitConverter.ToInt32(length, 0);
+
+// Read the data
+int bytesLeft = dataLength;
+byte[] data = new byte[dataLength];
+
+while (bytesLeft > 0)
+{
+
+    int nextPacketSize = (bytesLeft > bufferSize) ? bufferSize : bytesLeft;
+
+    bytesRead = netStream.Read(data, allBytesRead, nextPacketSize);
+    allBytesRead += bytesRead;
+    bytesLeft -= bytesRead;
+
+}
+
+// Save image to desktop
+File.WriteAllBytes("C: \\Users\\BI1335\\Desktop\\Socket\\Tcpimage.jpg", data);
+
+// Clean up
+netStream.Close();
+client.Close();
